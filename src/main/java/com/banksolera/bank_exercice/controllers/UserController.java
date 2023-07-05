a7b21f51088b2d57ff9488d67b6b74a16d49b5de
Pere Reynés <pere.reynes@Solera.com>
2023-07-05 13:10:47 +0200

    Created API and all the user part

---
+++ src/main/java/com/banksolera/bank_exercice/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.banksolera.bank_exercice.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.banksolera.bank_exercice.entities.User;
+import com.banksolera.bank_exercice.services.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class UserController {
+
+	private UserService userService;
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@PostMapping("/infobank")
+	public User createUser(@Valid @RequestBody User user) {
+		userService.createUser(user);
+		return user;
+	}
+}
---
+++ src/main/java/com/banksolera/bank_exercice/entities/InfoBank.java
@@ -0,0 +1,17 @@
+package com.banksolera.bank_exercice.entities;
+
+import com.banksolera.bank_exercice.entities.User;
+
+public class InfoBank {
+	Double moneyAccount;
+	String accountNum;
+	
+	public InfoBank(double moneyAccount, String accountNum) {
+		super();
+		this.moneyAccount = moneyAccount;
+		this.accountNum = accountNum;
+	}
+	
+	
+	
+}
