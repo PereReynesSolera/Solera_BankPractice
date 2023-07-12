076a310f905ed4988fef1309a531065c33baf900
Aritz Silva <aritz.silva@solera.com>
2023-07-12 16:43:21 +0200

    "Another one bite the dust"

--- src/components/Bank_Accounts/BankAccounts.js
+++ src/components/Bank_Accounts/BankAccounts.js
@@ -17,17 +17,26 @@ const BankAccounts = () => {
             <div className="Izq">
               <h2 className="headerdos">Bank Accounts</h2>
               {accounts.length <= 0 ? (
-                  <p>No available accounts</p>
+                <div className="p-button">
+                    <p>No available accounts</p>
+                    <button type="button" className="delete">Erase this button</button>
+                </div>
+
                 ) : (
                   accounts.map((account) => {
-                    return <p>{account}</p>;
+                    return (
+                    <div className="p-button">
+                        <p>{account}</p>
+                        <button type="button" className="delete">Delete</button>
+                    </div>
+                    );
                   })
                 )
               }
             </div>
             <div className="Der">
               <button type="button" className="create">Create</button>
-              <button type="button" className="delete">Delete</button>
+              {/*<button type="button" className="delete">Delete</button>*/}
             </div>
           </div>
         </div>
